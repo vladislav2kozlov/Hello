0d3c9dc070f9ca72de518b97c47e1af2b27cd27a
Vlad Kozlov <Vlad Kozlov>
2017-03-25 11:01:46 -0700

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello");
+
+            System.Console.WriteLine("Vladislav Valeriy Kozlov");
+
+        
+
+            System.Console.WriteLine("I'll be getting better and better!");
+
+            System.Console.WriteLine("Awesome!");
+
+            Console.ReadLine();
+        }
+    }
+}
